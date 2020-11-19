62991851bc52ad896f894f40974e386efa2b120d
Christopher Liu <christopherliu@sorcero.com>
2020-11-19 18:27:09 -0500

    Add copyright at footer

--- templates.py
+++ templates.py
@@ -3,13 +3,18 @@ from string import Template
 from config import *
 
 template_dir = TEMPLATE_DIR
+copyright = '''
+<footer>
+    <p class="has-text-centered"><small>Copyright &copy; 2020 Christopher Liu. All rights reserved.</small></p>
+</footer>
+'''
 
 def render_template(path, replacements={}):
     f = open(os.path.join(template_dir, path))
     s = f.read()
     f.close()
 
-    return Template(s).safe_substitute(replacements)
+    return Template(s).safe_substitute(replacements) + copyright
 
 def redirect(page):
-    return render_template('redirect.html', {'page': page})
+    return render_template('redirect.html', {'page': page}) + copyright
