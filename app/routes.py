85bb5de8c8ef8f6b0236627df48cc112283b3276
Christopher Liu <christopherliu@sorcero.com>
2020-12-21 23:18:57 -0500

    Add login form and page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -56,3 +57,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', date='Monday, December 14, 2020', day='Day A (1-5)', user=user, homeworks=homeworks, tests=tests, schedule=schedule)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Log In', form=form)
