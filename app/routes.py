b7b8ba7a61d72b086ec00cf2984cea7690d5c02b
Christopher Liu <christopherliu@sorcero.com>
2020-12-26 11:47:18 -0500

    Add link generation and navbar

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -64,5 +64,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Log In', form=form)
