dd4f48f3c6fc8fb71e422af058593295958d9000
Christopher Liu <christopherliu@sorcero.com>
2020-12-26 15:45:12 -0500

    Add homework table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -8,3 +9,15 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)  
+
+class Homework(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(64))
+    name = db.Column(db.String(64))
+    due_date = db.Column(db.DateTime, index=True, default=datetime(2020, 9, 28))
+    submit = db.Column(db.String(64))
+    done = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Homework {}>'.format(self.name)
