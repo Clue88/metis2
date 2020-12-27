3ee9d98cc6eec88429cddf38572581c0afdc8db6
Christopher Liu <christopherliu@sorcero.com>
2020-12-27 08:46:25 -0500

    Add classes to user model

--- app/models.py
+++ app/models.py
@@ -9,6 +9,56 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    period_1a = db.Column(db.String(256))
+    period_1a_zoom = db.Column(db.String(256))
+    period_1b = db.Column(db.String(256))
+    period_1b_zoom = db.Column(db.String(256))
+
+    period_2a = db.Column(db.String(256))
+    period_2a_zoom = db.Column(db.String(256))
+    period_2b = db.Column(db.String(256))
+    period_2b_zoom = db.Column(db.String(256))
+
+    period_3a = db.Column(db.String(256))
+    period_3a_zoom = db.Column(db.String(256))
+    period_3b = db.Column(db.String(256))
+    period_3b_zoom = db.Column(db.String(256))
+
+    period_4a = db.Column(db.String(256))
+    period_4a_zoom = db.Column(db.String(256))
+    period_4b = db.Column(db.String(256))
+    period_4b_zoom = db.Column(db.String(256))
+
+    period_5a = db.Column(db.String(256))
+    period_5a_zoom = db.Column(db.String(256))
+    period_5b = db.Column(db.String(256))
+    period_5b_zoom = db.Column(db.String(256))
+
+    period_6a = db.Column(db.String(256))
+    period_6a_zoom = db.Column(db.String(256))
+    period_6b = db.Column(db.String(256))
+    period_6b_zoom = db.Column(db.String(256))
+
+    period_7a = db.Column(db.String(256))
+    period_7a_zoom = db.Column(db.String(256))
+    period_7b = db.Column(db.String(256))
+    period_7b_zoom = db.Column(db.String(256))
+
+    period_8a = db.Column(db.String(256))
+    period_8a_zoom = db.Column(db.String(256))
+    period_8b = db.Column(db.String(256))
+    period_8b_zoom = db.Column(db.String(256))
+
+    period_9a = db.Column(db.String(256))
+    period_9a_zoom = db.Column(db.String(256))
+    period_9b = db.Column(db.String(256))
+    period_9b_zoom = db.Column(db.String(256))
+
+    period_10a = db.Column(db.String(256))
+    period_10a_zoom = db.Column(db.String(256))
+    period_10b = db.Column(db.String(256))
+    period_10b_zoom = db.Column(db.String(256))
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
